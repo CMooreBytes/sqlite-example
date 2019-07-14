d8a13a00396610b312d47806861110482f9f65e8
Devon <devongarrett@Asgard-MacBook.wowway.com>
2019-07-13 23:46:50 -0400

    .

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import(
 	"log"
-	"github.com/cmoorebytes/sqlite/pkg/datacontext"
+	"github.com/cmoorebytes/sqlite-example/pkg/datacontext"
 )
 
 
 func main(){
+	p := datacontext.GetPerson(1)
+	log.Printf("id %d: Assignee: %s, %s", p.Id, p.LastName, p.FirstName)
+	
 	s := datacontext.GetSchedule(1)
 	for _, a := range s {
 		log.Printf("Date %s: Assignee: %s, %s", a.Service.ServiceDate, a.Assignee.LastName, a.Assignee.FirstName)
 	}
-	
 }
